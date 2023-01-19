88e6be22fd102950b553479b31cd347d92c91061
Alex Zaitsev <alex.zaitsev.pro@gmail.com>
2023-01-19 13:21:40 +0100

    init

---
+++ app/src/main/java/com/alexzaitsev/modern/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.alexzaitsev.modern.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.alexzaitsev.modern.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
