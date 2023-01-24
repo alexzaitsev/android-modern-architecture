84ffb65eccb1b6f125592b81be3a4d007541e9b7
Alex Zaitsev <alex.zaitsev.pro@gmail.com>
2023-01-24 18:07:02 +0100

    fixed import

--- app/src/main/java/com/alexzaitsev/modern/view/MainViewModel.kt
+++ app/src/main/java/com/alexzaitsev/modern/view/MainViewModel.kt
@@ -3,12 +3,13 @@ package com.alexzaitsev.modern.view
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.alexzaitsev.modern.domain.usecase.GetDataWithLogicAppliedUseCase
 import kotlinx.coroutines.launch
 import org.koin.android.annotation.KoinViewModel
 
 @KoinViewModel
 class MainViewModel(
-    private val getDataWithLogicAppliedUseCase: com.alexzaitsev.modern.domain.usecase.GetDataWithLogicAppliedUseCase
+    private val getDataWithLogicAppliedUseCase: GetDataWithLogicAppliedUseCase
 ) : ViewModel() {
 
     fun getData() {
