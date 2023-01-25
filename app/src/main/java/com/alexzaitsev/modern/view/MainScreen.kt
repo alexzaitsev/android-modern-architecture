f75b911c649786a12dcbe30b5f54120b3422018a
Alex Zaitsev <alex.zaitsev.pro@gmail.com>
2023-01-25 16:59:04 +0100

    finished dummy main screen

--- app/src/main/java/com/alexzaitsev/modern/view/MainScreen.kt
+++ app/src/main/java/com/alexzaitsev/modern/view/MainScreen.kt
@@ -1,12 +1,37 @@
 package com.alexzaitsev.modern.view
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import com.alexzaitsev.modern.view.compose.theme.MyTheme
-import org.koin.androidx.compose.koinViewModel
 
 @Composable
 fun MainScreen() = MyTheme {
-    val viewModel = koinViewModel<MainViewModel>()
+    val scaffoldState = rememberScaffoldState()
 
-    viewModel.getData()
+    Scaffold(
+        backgroundColor = Color.Transparent,
+        scaffoldState = scaffoldState,
+        snackbarHost = { scaffoldState.snackbarHostState }
+    ) { padding ->
+        ScaffoldBody(padding)
+    }
+}
+
+@Composable
+private fun ScaffoldBody(
+    innerPadding: PaddingValues,
+) = Box(
+    modifier = Modifier
+        .fillMaxSize()
+        .padding(innerPadding)
+) {
+    Text("Main screen content")
 }
